7cb9fa120ae5ba9b59f3cc9a1717c789371e6630
simo <mohamedoukhajou@gmail.com>
2024-10-07 14:08:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const body = document.getElementsByTagName("body")[0];
+const colorCode = document.getElementById("color-code");
+
+function setColor(name) {
+    body.style.backgroundColor = name;
+    colorCode.textContent = name.toUpperCase();
+}
+
+function randomColor() {
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+    const color = `rgb(${red}, ${green}, ${blue})`;
+    body.style.backgroundColor = color;
+    colorCode.textContent = color;
+}
+
+
+randomColor();
